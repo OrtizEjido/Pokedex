3d88132ef3bac525cc48249f47b7b84589088107
OrtizEjido <67447680+OrtizEjido@users.noreply.github.com>
2020-08-04 15:42:15 -0700

    Creacion de paquetes y clase Pokemon.kt

---
+++ app/src/main/java/com/example/pokedex/model/Pokemon.kt
@@ -0,0 +1,14 @@
+package com.example.pokedex.model
+
+class Pokemon {
+
+    companion object Tipo{
+        var NORMAL = "Normal"
+    }
+
+    var id: Int? = null
+    var nombre: String? = null
+    var imageUrl: String? = null
+
+
+}
