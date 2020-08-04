522d888cc2d017d3beefdb8f928458f4ce38cf84
OrtizEjido <67447680+OrtizEjido@users.noreply.github.com>
2020-08-04 16:03:02 -0700

    Terminacion de clase Pokemon.kt

--- app/src/main/java/com/example/pokedex/model/Pokemon.kt
+++ app/src/main/java/com/example/pokedex/model/Pokemon.kt
@@ -2,12 +2,30 @@ package com.example.pokedex.model
 
 class Pokemon {
 
-    companion object Tipo{
+    companion object{
         var NORMAL = "Normal"
+        var ACERO = "Acero"
+        var FANTASMA = "Fantasma"
+        var AGUA = "Agua"
+        var FUEGO = "Fuego"
+        var PLANTA = "Planta"
+        var ROCA = "Roca"
+        var TIERRA = "Tierra"
+        var HADA = "Hada"
+        var HIELO = "Hielo"
+        var VENENO = "Veneno"
+        var LUCHA = "Lucha"
+        var OSCURO = "Oscuro"
+        var DRAGON = "Dragon"
+        var VOLADOR = "Volador"
+        var ELECTRICO = "Electrico"
+        var BICHO = "Bicho"
     }
 
     var id: Int? = null
     var nombre: String? = null
     var imageUrl: String? = null
+    var tipo: String? = null
+    var tipo2: String? = null
 
 }
