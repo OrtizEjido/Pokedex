b76626bef4feb623abbca0e5af4aa7cf75fed352
OrtizEjido <67447680+OrtizEjido@users.noreply.github.com>
2020-08-03 19:40:33 -0700

    alo

---
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pokedex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//Proyecto de prueba para sincronizacion con git
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
