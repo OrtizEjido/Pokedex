b4517b962c9a944afd7a904d896c3eb3b5692410
OrtizEjido <67447680+OrtizEjido@users.noreply.github.com>
2020-08-03 19:50:07 -0700

    Putos todos

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 //Proyecto de prueba para sincronizacion con git
-
+//HeatWave
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
